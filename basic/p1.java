ed3003304e2b3ff98fe1ecccccf5a79405cf769c
mean <meanstack4@gmail.com>
2025-07-05 17:19:51 +0530

    understaning System.out.println()

---
+++ basic/p1.java
@@ -0,0 +1,31 @@
+/*
+   .class
+   Java Class
+   Inner Class
+   Annonymous Inner
+   Interface
+   Enum
+
+   method---> class----> package
+
+   lang :-- it is default package of java
+   System :- It is built-in java class available in lang package
+   out :- It is an object of PrintStream, static member of 
+   System class.
+   println,print,printf :- These are all the instance method of
+   PrintStream Class.
+   How to call instance method :-  Using object
+ */
+class Test{
+    public static void main(String args[]){
+        System.out.println("Java is very simple..");
+        System.out.print("Hello...");
+        System.out.print("World...");
+        System.out.printf("InfoBeans Foundation");
+        System.err.println("Error Message");
+    }
+}
+
+
+
+
---
+++ basic/p2.java
@@ -0,0 +1,17 @@
+/*
+  Escape Sequence Character
+  \n -- new line
+  \t -- tab space
+  \r -- Carriage return
+  \b -- backspace
+  \a -- alarm | beep
+ */
+class Test{
+    public static void main(String args[]){
+      System.out.print("A\n");
+      System.out.print("B\n");
+      System.out.print("C\n");
+      System.out.println("\\n \\t \\a \\r \\b \\z"); 
+      System.out.println(" \"Welcome\" ");
+    }
+}
